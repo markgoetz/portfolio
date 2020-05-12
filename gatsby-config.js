e5dcfdbcee6e00ad9d59aaa238834c734c77c679
Mark Goetz <breakawayblue@gmail.com>
2020-05-11 21:48:32 -0500

    Update plugins; update description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Mark Goetz`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Portfolio of Mark Goetz, a web developer in Chicago.  Designy outside, mathy inside.`,
     author: `@markdoesnttweet`,
   },
   plugins: [
