7f6ca8305747a4416cfbe6eefefad5b46a20cc4a
Mark Goetz <breakawayblue@gmail.com>
2026-09-09 22:34:12 -0400

    Add all posts page

--- astro/src/utils/getPosts.ts
+++ astro/src/utils/getPosts.ts
@@ -3,8 +3,10 @@ import type { BlogPost } from "../types/blogPost";
 const getPosts = (): BlogPost[] => {
   const posts = Object.values(
     import.meta.glob("../pages/posts/*.md", { eager: true }),
+  ) as BlogPost[];
+  return posts.toSorted((a, b) =>
+    b.frontmatter.published.localeCompare(a.frontmatter.published),
   );
-  return posts as BlogPost[];
 };
 
 export default getPosts;
